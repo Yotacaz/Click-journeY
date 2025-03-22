9d48de5a32619f388cd522cedd38d31c02f0afaf
bina0413 <binetamhdiop@gmail.com>
2025-03-22 21:55:18 +0100

    Update voyages_mieux_notes.php

--- php/voyages_mieux_notes.php
+++ php/voyages_mieux_notes.php
@@ -1,43 +1,67 @@
+<?php
+// Charger les données du fichier JSON
+require_once "php-include/fonctions_voyages.php";
+$voyages= decodageDonnees("../donnees/voyage/voyages.json");
+$identifiant=recup_id();
+
+if(is_array($voyages)){ //decodage reussi on peut trier
+$titre_page= $voyages[$identifiant]["titre"];}
+    else{     // Si ce n'est pas un tableau, afficher le message d'erreur
+        echo $voyages;
+        exit();
+    }
+    $v= array_splice($voyages ,$identifiant,1);  //ne garder que le jeu de la page concernee  
+    $v=$v[0];
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <head>
     <meta name="auteur" content="DIOP Bineta" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
     <link rel="icon" type="image/x-icon" href="../img/logo.png">
-    <meta name="description" content="Page de présentation du site et recherche rapide" />
-    <title>Les mieux notes - PixelTravels</title>
-</head>
-</head>
+    <meta name="description" content="Page de présentation du site et recherche rapide" />   
+    <title><?php echo "$titre_page"; ?></title>
 <body>
 <?php
-require_once "php-include/fonctions_voyages.php";
-require_once "php-include/header.php";
-$fichier = "../donnees/voyage/voyages.json";
+    require_once "php-include/header.php";
+    ?>
+<br><br><br><h1>Récapitulatif du voyage</h1>
 
-$voyages = trierVoyage($fichier);
+<!-- Affichage des informations générales du voyage -->
+<div class="voyage-details">
+    <h2><?php echo $v['titre']; ?> (Note: <?php echo $v['note']; ?>)</h2>
+    <p><strong>Description:</strong> <?php echo $v['description']; ?></p>
+    <p><strong>Dates:</strong> <?php echo $v['dates']['debut']; ?> - <?php echo $v['dates']['fin']; ?></p>
+    <p><strong>Durée:</strong> <?php echo $v['dates']['duree']; ?> jours</p>
+</div>
 
-//boucle 5 premiers voyages
-$voyages = array_splice($voyages, 0, 5);
+<h3>Étapes du voyage</h3>
 
-// Parcourir les 5 premiers éléments
-    echo '<div class="mieux_notes">';
-    echo "<br><br><br><center><h1>LES MIEUX NOTES<h1/></center>";
+<?php
+foreach ($v['etapes'] as $etape) {
+    echo '<div class="etape">';
+    echo '<h4>' . $etape['nom'] . '</h4>';
+    echo '<p><strong>Dates:</strong> ' . $etape['dates']['debut'] . ' - ' . $etape['dates']['fin'] . '</p>';
+    echo '<p><strong>Durée:</strong> ' . $etape['dates']['duree'] . ' jours</p>';
 
-foreach ($voyages as $v) {
-	$index=(int)$v['id'];
-	$index--;
-echo '<div class="info_voyage">
-    <h2><a href="details_voyage.php?id='.$index.'">Voyage: ' . $v['titre'] . '</a></h2>
-	   <p><strong>Note:</strong> ' . $v['note'] .' / 5</p>
-	   <p><strong>Description:</strong> ' . $v['description'] . '</p>
-	   	<p><strong>Durée:</strong> ' . $v['dates']['duree'] . ' jours</p></h5>
-    <hr>
-    </div>';
+    // Affichage des options modifiées
+    foreach ($etape['options'] as $option) {
+        echo '<p><strong>' . $option['nom'] . ':</strong> ' . $option['nombre_personnes'] . ' personnes choisies';
+        if (isset($option['valeur_choisie'])) {
+            echo ' (Option choisie: ' . $option['valeur_choisie'] . ')';
+        }
+        echo '</p>';
+    }
+    echo '</div>';
 }
-echo "</div>";
+require_once "php-include/footer.php";
+?>
+</body>
+</html>
+
 
 ?>
     require_once "php-include/footer.php";	
