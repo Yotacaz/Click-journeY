956850b5413b930d0e98927358c80dc7cb1177a7
AugustinAveline <augustin.aveline@gmail.com>
2025-03-23 19:48:41 +0100

    Add files via upload

---
+++ php/paiement.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Paiement voyage - PixelTravels</title>
+    <meta name=”auteur” content=”Augustin Aveline” />
+    <meta name=”description” content=”page de paiement au site” />
+    <link rel="stylesheet" type="text/css" href="../style.css" />
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    require_once "php-include/header.php";
+    require_once "php-include/getapikey.php";
+    $date = date("Y-m-d");
+    $info = json_decode(file_get_contents("../donnees/paiement/transaction_en_cours.json"), true);
+    $passage= array("a","z","e","r","t","y","u","i","o","p","q","s","d","f","g","h","j","k","l","m","w","x","c","v","b","n","0","1","2","3","4","5","6","7","8","9","A","Z","E","R","T","Y","U","I","O","P","Q","S","D","F","G","H","J","K","L","M","W","X","C","V","B","N");
+    $nb1 = rand(10,24);
+    $vendeur="MI-3_A";
+    //ATTENTION
+    //ATTENTION
+    //le lien ne fonctionne que si le fichier mère du projet s'appelle "main". Si ce n'est pas votre cas merci de changer le chemin par le chemin reliant "localhost" aux dossier "php".
+    $retour="http://localhost/main/php/retour_transaction.php";
+    //le lien ne fonctionne que si le fichier mère du projet s'appelle "main". Si ce n'est pas votre cas merci de changer le chemin par le chemin reliant "localhost" aux dossier "php".
+    //ATTENTION 
+    //ATTENTION
+    $transaction=$passage[rand(0,61)];
+    for($i=0;$i<$nb1 -1;$i++){
+        $transaction=$passage[rand(0,61)].$transaction;
+    }
+    $api_key = getAPIKey("$vendeur");
+    $control=md5($api_key."#".$transaction."#".$info["montant"]."#".$vendeur."#".$retour."#");
+    ?>
+    
+    <main>
+        <div class="bandeau">
+            <h1>
+                Page de paiement
+            </h1>
+        </div>
+        <div class="conteneur-image-texte">
+            <div class="bloc" class="conteneur-texte">
+                <h2>Coordonnée bancaire</h2><br>
+                <form class="grille3" action='https://www.plateforme-smc.fr/cybank/index.php' method="post" name="paiement">
+
+                    <label for="num_carte" class="col1">Num Carte : </label>
+                    <input class="col2" type="text" name="num_carte" id="num_carte" contenteditable="false" placeholder="Num carte bancaire"><br />
+
+                    <label for="proprio" class="col1">Nom-Prénom propriétaire carte :</label>
+                    <input class="col2" type="text" name="proprio_carte" id="proprio" contenteditable="false" placeholder="nom-prénom propriétaire carte"></br>
+
+                    <label class="col1" for="date">Date d'expiration:</label>
+                    <input class="col2" type="date" name="date_expiration" id="date" min="<?php echo "$date"; ?>" ><br />
+
+                    <label for="valcontrole" class="col1">Valeur de contrôle :</label>
+                    <input class="col2" type="text" name="valeur_controle" id="valcontrol" contenteditable="false" placeholder="valeur de contrôle"><br />
+
+                    <input type='hidden' name='transaction' value="<?php echo "$transaction" ?>">
+                    <input type='hidden' name='montant' value="<?php echo "$info[montant]" ?>">
+                    <input type='hidden' name='vendeur' value="<?php echo "$vendeur" ?>">
+                    <input type='hidden' name='retour' value="<?php echo "$retour" ?>">
+                    <input type='hidden' name='control' value="<?php echo "$control" ?>">
+
+                    <p class="col1"> </p>
+                    <label class="col2">
+                        <button type="submit" name="boutton" class="submit">validé et payé</button>
+                    </label>
+                </form>
+
+                <?php
+                    /*if(isset($_POST["boutton"])){
+                        $num_carte=$_POST['num_carte'];
+                        $valeur=$_POST['valeur_controle'];
+                        if(strlen($num_carte) != 16){
+                            echo "Merci de rentré un numéros de carte valide.";
+                        }
+                        else if(strlen($valeur) != 3){
+                            echo "Merci de rentré une valeur de contrôle valide.";
+                        }
+                    }*/
+                    $cobanc=array(/*"num_carte"=> $_POST['num_carte'] ,*/ "proprietaire"=>"bob bob" , "dates_expirations" => "2029-01-01" , "valeur_controle"=>"555");
+                    $voyage=array('nom_voyage' => "GTA V",'nb_personne' => 6,'dates_debut' => "2028-12-20" ,'date_fin' => "2029-01-05");
+                    $temp= array('montant' => 1800.00 , "utilisateur" => "bob@bob.bob" ,"voyage"=> $voyage , "cobancaire"=> $cobanc);
+                    $open = fopen("../donnees/paiement/transaction_en_cours.json",'w');
+                    fwrite( $open , json_encode($temp));
+                    fclose($open);
+                ?>
+
+            </div>
+            <div>
+                <div class="conteneur-texte">
+                    <h2>Recapitulatif</h2>
+                    Vous participé au voyage <em><?php echo $info["voyage"]["nom_voyage"] ?></em> à <em><?php echo $info["voyage"]["nb_personne"] ?></em> personnes.</br>
+                    Le voyage débuteras le <em><?php echo $info["voyage"]["dates_debut"] ?></em> et finiras le <em><?php echo $info["voyage"]["date_fin"] ?></em>.</br>
+                    Pour un montant totale de <em><?php echo "$info[montant]€" ?></em>.
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php
+    require_once "php-include/footer.php";
+    ?>
+</body>
+
+</html>
---
+++ php/retour_transaction.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Paiement refusé - PixelTravels</title>
+    <meta name=”auteur” content=”Augustin Aveline” />
+    <meta name=”description” content=” paiement refusé” />
+    <link rel="stylesheet" type="text/css" href="../style.css" />
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
+    <meta charset="UTF-8">
+</head>
+
+<?php
+    $info = json_decode(file_get_contents("../donnees/paiement/transaction_en_cours.json"), true);
+    $date=date("j_F_Y");
+    $status=$_GET["status"];
+    if($status == "accepted"){
+        $transaction=$_GET["transaction"];
+        $vendeur=$_GET["vendeur"];
+        $montant=$_GET["montant"];
+        $info_transaction=array("date_transaction" => "$date" , "ID_transaction" => "$transaction" , "vendeur" => "$vendeur" , "montant" => "$montant");
+        $tracabilite=array("compte utilisateur" => "$info[utilisateur]" , "Info_transaction" => $info_transaction , "Voyage" => $info["voyage"] , "Coordonnee_Bancaire" => $info["cobancaire"]);
+        $open = fopen("../donnees/paiement/transaction_finis.json",'a+');
+        fwrite( $open , json_encode($tracabilite));
+        fclose($open);
+        header("Location: accueil.php");
+    }
+?>
+
+<body>
+    <?php
+        require_once "php-include/header.php";
+    ?>
+    
+    <main>
+        <div class="bandeau">
+            <h1>
+                Paiement refusé
+            </h1>
+        </div>
+        <div class="conteneur-image-texte">
+            <div class="bloc" class="conteneur-texte">
+                <h2>paiment refusé par manque d'argent sur le compte</h2><br>
+                    <div>
+                        Si vous voulez tentez à nouveaux de payez :
+                    </div></br>
+                    <a href="paiement.php" class="lien">ICI</a></br></br>
+                    <div>
+                        Si vous voulez changer des options pour réduire le prix :
+                    </div></br>
+                    <a href="connexion.php" class="lien">ICI</a>
+            </div>
+        </div>
+    </main>
+    <?php
+    require_once "php-include/footer.php";
+    ?>
+</body>
+
+</html>
