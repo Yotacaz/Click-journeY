907b822508e3881ebc798bb5df8e3c8101f860f5
bina0413 <binetamhdiop@gmail.com>
2025-03-23 21:23:25 +0100

    Update modif_voyage.php

--- php/modif_voyage.php
+++ php/modif_voyage.php
@@ -14,19 +14,18 @@ if (!isset($voyages[$identifiant])) {
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+echo "il y a bien post";
 
     // Charger les données existantes du fichier JSON
     $voyages= decodageDonnees("../donnees/voyage/voyages.json");
     if (!is_array($voyages)) {
-    die("Erreur: Impossible de charger les données des voyages.");
+    die("erreur impossible de charger les données des voyages.");
 }
     $identifiant=recup_id();
     if (!isset($voyages[$identifiant])) {
-    die("Erreur: Identifiant voyage non trouvé.");
+    die("erreur pas d'dentifiant voyage");
 }
 
-    // Parcourir chaque étape et option pour récupérer les modifications envoyées
     foreach ($voyages[$identifiant]['etapes'] as $etape_index => &$etape) {
         foreach ($etape['options'] as $option_index => &$option) {
             // Récupérer la valeur sélectionnée et le nombre de personnes
@@ -41,14 +40,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST[$nombre_personnes_nom])) {
                 $option['nombre_personnes'] = $_POST[$nombre_personnes_nom];  // Enregistrer le nombre de personnes
             }
+            echo $_POST[$nombre_personnes_nom];
         }
     }
 
-    // Sauvegarder les modifications dans le fichier JSON
-    file_put_contents("../donnees/voyage/voyages.json", json_encode($voyages, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    // Sauvegarder les modifications dans le fichier JSON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    if (file_put_contents("../donnees/voyage/voyages.json", json_encode($voyages, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)) === false) {
+    die("Erreur: Impossible d'écrire dans le fichier JSON !");
+}
 
     // Rediriger vers la page récapitulative
     header("Location: recap_voyage.php?id=".$identifiant."");
     exit;
-}
+}else echo "pas de POST";
 ?>
