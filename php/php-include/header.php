e0b245e3695ebbffb98f1e61fabec39a97abdc51
AugustinAveline <augustin.aveline@gmail.com>
2025-05-04 19:03:50 +0200

    Update header.php

--- php/php-include/header.php
+++ php/php-include/header.php
@@ -69,7 +69,7 @@ $page_active = basename(path: htmlspecialchars($_SERVER["PHP_SELF"]));
                         echo '>Inscription</a>';
                     }
                     ?>
-                    <span><label for="theme"><input type="checkbox" name="theme" id="theme" value="TODO">&nbsp;Thème xxx</label></span>
+                    <span><label for="theme"><input type="checkbox" name="theme" id="theme" value="TODO">&nbsp;Thème clair</label></span>
                 </div>
             </div>
         </nav>
