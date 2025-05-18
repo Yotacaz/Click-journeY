11fe66dde28e14d568975fa842c630f17aac1499
AugustinAveline <augustin.aveline@gmail.com>
2025-05-18 09:55:52 +0200

    Passage au fetch
    Manque que le code coté PHP que je n'arrive pas à faire pour vérifier que celui-ci fonctionne (j'ai copié le cours donc c'est sensé fonctionné)

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,38 @@
-
 let btn_modif = document.getElementsByClassName("modif-utilisateur");
+
 for (let i = 0; i < btn_modif.length; i++) {
-    btn_modif[i].addEventListener("click", function () {
+    btn_modif[i].addEventListener("click", function (e) {
+        e.preventDefault();
         let id_form = this.getAttribute("form");
+        let form = document.forms[id_form];
+
         this.style.backgroundColor = "gray";
         this.style.color = "white";
         this.innerHTML = "En cours...";
         this.setAttribute("disabled", "disabled");
         document.body.style.cursor = "wait";
-        setTimeout(() => {
-            document.forms[id_form].submit();
-        }, 2000);
 
+        // Attente simulée
+        setTimeout(() => {
+            let formData = new FormData(form);
+
+            fetch("../php/php-form/Fetch_admin.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log("Réponse du serveur :", data);
+                this.innerHTML = "Modifié";
+                this.style.backgroundColor = "green";
+                document.body.style.cursor = "default";
+            })
+            .catch(error => {
+                console.error("Erreur :", error);
+                this.innerHTML = "Erreur";
+                this.style.backgroundColor = "red";
+                document.body.style.cursor = "default";
+            });
+        }, 2000);
     });
 }
